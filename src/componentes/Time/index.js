35045d12fbc2b49e92956b27970f0d84220e1f26
gustavobraga1001 <gubraro0@gmail.com>
2023-07-12 18:50:49 -0300

    mudando as cores dos times dinamicamente

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,11 +1,12 @@
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores, aoDeletar}) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor}) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 
         'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input value={time.corSecundaria} onChange={evento => mudarCor(evento.target.value, time.nome)} type='color' className='input-cor' />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
