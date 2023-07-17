9c1f81ef89e8b5ab91b955254d452a88d3fd5e99
gustavobraga1001 <gubraro0@gmail.com>
2023-07-17 14:20:57 -0300

    importando uuid e colocanco ids nos colaboradores

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,8 +6,8 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor}) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 
-        'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.5 ') }}>
-            <input value={time.cor} onChange={evento => mudarCor(evento.target.value, time.nome)} type='color' className='input-cor' />
+        'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.5 ') }}> 
+            <input value={time.cor} onChange={evento => mudarCor(evento.target.value, time.id)} type='color' className='input-cor' />
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
